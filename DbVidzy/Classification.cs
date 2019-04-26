57333310be20f8b4719a5ff505d9578df3fbb441
zerodie7 <zerodie.7@hotmail.com>
2019-04-26 17:23:14 -0500

    DB EntityFramework

---
+++ DbVidzy/Classification.cs
@@ -0,0 +1,11 @@
+﻿namespace Vidzy
+{
+    using System;
+
+    public enum Classification : byte
+    {
+        Silver = 1,
+        Gold = 2,
+        Platinum = 3
+    }
+}
---
+++ DbVidzy/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vidzy;
+
+namespace DbVidzy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dbCont = new VidzyDbCont();
+            dbCont.AddVideo("Kill la kill", DateTime.Today, "Family", (byte)Classification.Gold);
+            dbCont.AddVideo("Kill Bill 2", DateTime.Today, "Comedy", (byte)Classification.Platinum);
+            dbCont.AddVideo("Kill Bill 3", DateTime.Today, "Romance", (byte)Classification.Silver);
+
+            Console.WriteLine("Fin");
+        }
+    }
+}
+
